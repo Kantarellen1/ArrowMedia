683c4b43c70d3f5d0057456b38f033877c600216
Kasper Korthsen <Kalletheman@live.dk>
2024-01-23 13:23:31 +0100

    Added Xml file to start working on the singular tabs so that it will load the data inputed in from the Xml data and made it compatiable, still some errors that need to be ironed out bust the main thing is done

--- LoginForm/Form2.cs
+++ LoginForm/Form2.cs
@@ -4,18 +4,55 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Forms;
+using System.Xml;
+
 
 namespace LoginForm
 {
     public partial class Interface : Form
     {
         bool sidebarExpand;
+        XmlDocument xmlDoc = new XmlDocument();
         public Interface()
         {
             InitializeComponent();
+            LoadXmlData();
+        }
+
+        private void LoadXmlData()
+        {
+            try
+            {
+                string xmlFilePath = @"C:\Users\Kalle\source\repos\ArrowMedia\LoginForm\Frame.xaml";
+
+                xmlDoc.Load(xmlFilePath);
+
+                XmlNode rootElement = xmlDoc.SelectSingleNode(".//[local-name()='RootElement'");
+
+                if (rootElement != null)
+                {
+                    XmlNode childElement1 = rootElement.SelectSingleNode(".//*[local-name()='ChildElement1']");
+                    if (childElement1 != null)
+                    {
+                        textBox1.text = childElement1.InnerText;
+                    }
+                    XmlNode childElement2 = rootElement.SelectSingleNode(".//*[local-name()='ChildElemenet2'");
+                    if (childElement2 != null)
+                    {
+                        textBox2.text = childElement2.InnerText;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading XML data: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
@@ -45,7 +82,7 @@ namespace LoginForm
 
         private void HomeButton_Click(object sender, EventArgs e)
         {
-
+            LoadXmlData();
         }
 
         private void Interface_Load(object sender, EventArgs e)
